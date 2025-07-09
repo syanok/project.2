2b02bf38f84f7dd091495ac5d49cbdf44b6058e4
syanok <syanok>
2025-07-09 09:32:58 -0400

    adding JS code for toggle button

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
 const toggleButton = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
+
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
